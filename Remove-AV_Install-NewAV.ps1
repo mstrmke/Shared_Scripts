52f456ccf311188b4cdd52ef98ef5d37252f9815
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-01-19 17:20:43 -0600

    Try/Catch Block and 2nd AV added

--- Remove-AV_Install-NewAV.ps1
+++ Remove-AV_Install-NewAV.ps1
@@ -5,30 +5,42 @@
 	 Created by:   	Ryan Hogan
 	 Organization: 	Heartland Business Systems
 	 Filename:     	Remove-AV_Install-NewAV.ps1
-	 Version: 0.1.0 - 01/19/2023 - Initial Version
+	 Version: 	0.1.1 - 01/19/2023 - Added two AV component paths to check and Try/Catch for install to report error
+	 		0.1.0 - 01/19/2023 - Initial Version
 	===========================================================================
     .DESCRIPTION
-		This script confoigures BizTalk server for the 3E environment. It does not install or Configure the ESB for Biztalk. 
-		The script will walk prompt for continuation when manual steps need to be completed (See Installer Checklist)
+		This script uninstalls a current Antivirus, if exists, and installs a new Antivirus from the command desired. 
 #>
 
+$PathToAV1 = "<Path to Antivirus SentinalOne service executable>"
+$PathToAV2 = "<Path to Antivirus2 Webroot service executable>"
 
-$OSType = "$(((gcim Win32_OperatingSystem).Name).split(‘|’)[0])"
-
-$Path = "<Path to Antivirus client service executable>"
-If ($Path)
+If ($PathToAV1)
 {
-   #Uninstall Antivirus
-(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
+   	#Uninstall Antivirus1
+	(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
 }
 
-Else { 
-   start-process "cmd.exe" "/c "<.exe of new AV with any switches>"
+ElseIf ($PathToAV2)
+{
+	#Uninstall Antivirus2
+	(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
+}
+Else {
+   	Try
+	{
+	#Inastall New AV
+	#start-process "cmd.exe" "/c "<.exe of new AV with any switches>"
    
-   #If using MSI (Uncomment below and comment the command above)
-   #start-process "msiexec.exe /i "<Path of MSI setup>" /noreboot /qn
-    
-    Write-Output "AV Client already uninstalled, New Antivirus Installed"
+   	#If using MSI (Uncomment below and comment the command above)
+   	start-process "msiexec.exe /i "<Path of MSI setup>" /noreboot /qn
+   
+   	Write-Output "Old AV Client was already uninstalled, New Antivirus Installed Succesfully"
+	}
+	Catch 
+	{
+		Write-Output "Error in BitDefender Install, [Error].0"
+	}
 }
 
 Exit $LASTEXITCODE
