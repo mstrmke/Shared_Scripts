4b47d74e656a2e945c982620feca259da76e954a
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-01-20 08:58:51 -0600

    Update Remove-AV_Install-NewAV.ps1

--- Remove-AV_Install-NewAV.ps1
+++ Remove-AV_Install-NewAV.ps1
@@ -5,42 +5,51 @@
 	 Created by:   	Ryan Hogan
 	 Organization: 	Heartland Business Systems
 	 Filename:     	Remove-AV_Install-NewAV.ps1
-	 Version: 	0.1.1 - 01/19/2023 - Added two AV component paths to check and Try/Catch for install to report error
-	 		0.1.0 - 01/19/2023 - Initial Version
+	 Version: 		0.2.0 - 01/19/2023 - Moved Uninstall and Install to their own Functions. 
+	 				0.1.2 - 01/19/2023 - Moved Try/Catch out of Else statement 
+	 				0.1.1 - 01/19/2023 - Added two AV component paths to check and Try/Catch for install to report error
+	 				0.1.0 - 01/19/2023 - Initial Version
 	===========================================================================
     .DESCRIPTION
 		This script uninstalls a current Antivirus, if exists, and installs a new Antivirus from the command desired. 
 #>
 
-$PathToAV1 = "<Path to Antivirus SentinalOne service executable>"
-$PathToAV2 = "<Path to Antivirus2 Webroot service executable>"
+Function UninstallOldAV{
 
-If ($PathToAV1)
-{
-   	#Uninstall Antivirus1
-	(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
-}
+	#Call Full Paths to AV Service executables, ex: C:\SEP\someexecutable.exe
+	$PathToAV = "<Path to Antivirus1 service executable>","<Path to Antivirus2 service executable>"
 
-ElseIf ($PathToAV2)
-{
-	#Uninstall Antivirus2
-	(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
+
+	foreach ($Path in $PathToAV){
+	
+		If (Test-Path -path $Path -PathType Leaf)
+		{
+   			Try{
+				#Uninstall Antivirus1
+				(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus1 name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
+			}
+			Catch {
+				(Get-WmiObject -Class Win32_Product -Filter {Name='<Antivirus2 name in Add/Remove Programs>'} -ComputerName . ).Uninstall()
+			}	
+		}
+
+		Else {}
+	}
+	Write-Output "Old AV Client was already uninstalled, Installing New AV"
 }
-Else {
-   	Try
+Function InstallNewAV {
+	Try
 	{
-	#Inastall New AV
-	#start-process "cmd.exe" "/c "<.exe of new AV with any switches>"
-   
-   	#If using MSI (Uncomment below and comment the command above)
-   	start-process "msiexec.exe /i "<Path of MSI setup>" /noreboot /qn
-   
-   	Write-Output "Old AV Client was already uninstalled, New Antivirus Installed Succesfully"
+		#Inastall New AV, ex: C:\Temp\Antivirus.msi (No Spaces in path)
+		start-process msiexec.exe -Wait -ArgumentList '/i <Path of MSI setup> /noreboot /qn'
+   		Write-Output "New Antivirus Installed Succesfully"
 	}
 	Catch 
 	{
-		Write-Output "Error in BitDefender Install, [Error].0"
+		Write-Output "Error in Antivirus Install. Detail: $Error[0]"
 	}
 }
 
-Exit $LASTEXITCODE
+
+UninstallOldAV
+InstallNewAV
