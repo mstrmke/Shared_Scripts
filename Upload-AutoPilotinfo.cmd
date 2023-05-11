cc8f70a49f6a01567fc5a5a67d12e2f387204150
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-05-10 23:00:34 -0500

    Update Upload-AutoPilotinfo.cmd
    Added ExecutionPolicy to each line.

--- Upload-AutoPilotinfo.cmd
+++ Upload-AutoPilotinfo.cmd
@@ -1,6 +1,6 @@
 @echo on
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"
-powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}"
-powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
 powershell -Command "& {set-executionpolicy RemoteSigned}"
 powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}"
