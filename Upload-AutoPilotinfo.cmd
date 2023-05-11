b841e22b0f6742618320848e606d661d19e2a298
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-05-10 22:07:38 -0500

    Update Upload-AutoPilotinfo.cmd

--- Upload-AutoPilotinfo.cmd
+++ Upload-AutoPilotinfo.cmd
@@ -3,4 +3,4 @@ powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.Secur
 powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}"
 powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"
 powershell -Command "& {set-executionpolicy RemoteSigned}"
-powershell -Command "& {Get-WindowsAutopilotInfo -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot -ExecutionPolicy bypass}"
+powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}"
