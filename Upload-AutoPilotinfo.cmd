4675dc7160ae5df2bfa1da046bd03e4db7343f98
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-05-11 11:21:08 -0500

    Update Upload-AutoPilotinfo.cmd
    Added bypassing the execution policy for each cmd and a restart.

--- Upload-AutoPilotinfo.cmd
+++ Upload-AutoPilotinfo.cmd
@@ -1,6 +1,8 @@
 @echo on
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"
-powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}"
-powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}" -executionpolicy bypass
+powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -executionpolicy bypass
+powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}" -executionpolicy bypass
 powershell -Command "& {set-executionpolicy RemoteSigned}"
-powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}"
+powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}" -executionpolicy bypass
+@echo off
+shutdown.exe -r -f -t 15
