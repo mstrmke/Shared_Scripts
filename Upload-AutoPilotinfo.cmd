e556be7270905d903597f7f7ab30d1e787a38b12
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-05-16 14:29:08 -0500

    Update Upload-AutoPilotinfo.cmd
    Updated Timeout to 20 seconds.

--- Upload-AutoPilotinfo.cmd
+++ Upload-AutoPilotinfo.cmd
@@ -1,7 +1,7 @@
 @echo on
 powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}" -ExecutionPolicy Bypass
 powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -ExecutionPolicy Bypass
-timeout /t 60 /nobreak
+timeout /t 20 /nobreak
 powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
 powershell -Command "& {set-executionpolicy RemoteSigned}"
 powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}" -ExecutionPolicy Bypass
