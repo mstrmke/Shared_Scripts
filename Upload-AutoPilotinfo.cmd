5fb2487bb00d3021fe04002661f8563b07ec30e8
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-06-01 08:59:51 -0500

    Update Upload-AutoPilotinfo.cmd
    Removed "-AddtoGroup"

--- Upload-AutoPilotinfo.cmd
+++ Upload-AutoPilotinfo.cmd
@@ -3,4 +3,4 @@ powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.Secur
 powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -ExecutionPolicy Bypass
 powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
 powershell -Command "& {set-executionpolicy RemoteSigned}"
-powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -AddToGroup 'Autopilot-Hybrid Azure AD Joined' -Reboot}" -ExecutionPolicy Bypass
+powershell -Command "& {Get-WindowsAutopilotInfo.ps1 -Online -Reboot}" -ExecutionPolicy Bypass
