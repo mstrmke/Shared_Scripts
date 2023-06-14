3905a1579c62f8af1b9ddff56acde707e8548a33
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-06-14 16:41:49 -0500

    Update Update-AutopilotLocation.ps1

--- Update-AutopilotLocation.ps1
+++ Update-AutopilotLocation.ps1
@@ -6,48 +6,25 @@
    Organization: 	Heartland Business Systems
    Filename:     	Update-AutopilotLocation.ps1
    Version:         1.0 (Initial Version)
+					1.1 - Remove AppX Bundle removal as it is not related. 
   ===========================================================================
   
   .DESCRIPTION
-    This script set the location to enabled so that the time zone is current. It also Removes any Windows Applications that are installed by Default. 
+    This script set the location to enabled so that the time zone is current.
  
 
 #>
 
-Create a tag file just so Intune knows this was installed
+#Create a tag file just so Intune knows this was installed
 if (-not (Test-Path "$($env:ProgramData)\Microsoft\AutopilotBranding"))
 {
     Mkdir "$($env:ProgramData)\Microsoft\AutopilotBranding"
 }
 Set-Content -Path "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.ps1.tag" -Value "Installed"
 
-# Start logging
-Start-Transcript "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.log"
+# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
+Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
 
-# PREP: Load the Config.xml
-$installFolder = "$PSScriptRoot\"
-Write-Host "Install folder: $installFolder"
-Write-Host "Loading configuration: $($installFolder)Config.xml"
-[Xml]$config = Get-Content "$($installFolder)Config.xml"
-
-
-if ($config.Config.TimeZone) {
-	Write-Host "Setting time zone: $($config.Config.TimeZone)"
-	Set-Timezone -Id $config.Config.TimeZone
-}
-else {
-	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
-	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
-}
-
-Write-Host "Removing specified in-box provisioned apps"
-$apps = Get-AppxProvisionedPackage -online
-$config.Config.RemoveApps.App | % {
-	$current = $_
-	$apps | ? {$_.DisplayName -eq $current} | % {
-		Write-Host "Removing provisioned app: $current"
-		$_ | Remove-AppxProvisionedPackage -Online | Out-Null
-	}
-}
+return 0
