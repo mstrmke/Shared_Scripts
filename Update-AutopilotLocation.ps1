4005da515b09d914f65785975ae722d45ebd115d
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-06-14 16:42:58 -0500

    Update Update-AutopilotLocation.ps1

--- Update-AutopilotLocation.ps1
+++ Update-AutopilotLocation.ps1
@@ -25,20 +25,8 @@ Set-Content -Path "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBran
 # Start logging
 Start-Transcript "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.log"
 
-# PREP: Load the Config.xml
-$installFolder = "$PSScriptRoot\"
-Write-Host "Install folder: $installFolder"
-Write-Host "Loading configuration: $($installFolder)Config.xml"
-[Xml]$config = Get-Content "$($installFolder)Config.xml"
 
-
-if ($config.Config.TimeZone) {
-	Write-Host "Setting time zone: $($config.Config.TimeZone)"
-	Set-Timezone -Id $config.Config.TimeZone
-}
-else {
 	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
 	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
-}
