6ad5e47688b1af52289db2902334a60f146955b8
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-07-27 19:47:55 -0500

    Create Upload-Autopilotinfo_HAADJ.ps1

---
+++ Upload-Autopilotinfo_HAADJ.ps1
@@ -0,0 +1,8 @@
+rem version 1.4 - For Hybrid Azure AD Joined Devices. Change "GROUPNAME" to the Hybrid Azure AD Domain Configuration Profile Group in InTine
+@echo on
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 -Force}" -ExecutionPolicy Bypass
+powershell -Command "& {Install-Script -name Get-WindowsAutopilotInfo -Force}"  -ExecutionPolicy Bypass
+powershell -Command "& {set-executionpolicy RemoteSigned}"
+
+powershell.exe -file "C:\Program Files\WindowsPowerShell\Scripts\Get-WindowsAutoPilotInfo.ps1" -Online -AddToGroup "GROUPNAME" -Assign
