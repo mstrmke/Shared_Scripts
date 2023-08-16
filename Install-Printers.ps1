4102978fb711733f526f0ea1086b3f50be00a35b
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-08-16 10:59:48 -0500

    Update Install-Printers.ps1

--- Install-Printers.ps1
+++ Install-Printers.ps1
@@ -1,22 +1,14 @@
 $WCSBW = Get-Printer -Name "\\WSDPS1\WDS-BW"
 $WCSCOLOR = Get-Printer -Name "\\WSDPS1\WDS-COLOR"
 
-If ($WCSBW){
-    Write-Host "B/W Printer was already installed."
-    Exit 0
-}
-Else {
+If (-not $WCSBW){
     (New-Object -ComObject WScript.Network).AddWindowsPrinterConnection("\\WSDPS1\WCS-BW")
     Write-Host "B/W Printer Installed!"
-    Exit 0
 }
-
-If ($WCSCOLOR){
-    Write-Host "Color Printer was already installed."
-    Exit 0
-}
-Else {
+Else { Write-Host "No Printer added, B/W Printer was already installed." }
+    
+If (-not $WCSCOLOR){
     (New-Object -ComObject WScript.Network).AddWindowsPrinterConnection("\\WSDPS1\WCS-COLOR")
     Write-Host "Color Printer Installed!"
-    Exit 0
 }
+Else { Write-Host "No Printer addedk, Color Printer was already installed." }
