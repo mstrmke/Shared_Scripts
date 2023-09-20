9ac21c49715ceb070a007dcfbeef6fb0455d6521
mstrmke <hogan.ryant@gmail.com>
2023-09-20 12:09:23 -0500

    New Script

---
+++ Update-OnPremUPN.ps1
@@ -0,0 +1,13 @@
+﻿#Specify UPN Domain
+$Domain = 'NewDOMAINUPN.com'
+ 
+#Get list of samaccountnames in our targeted OU
+$UserList = Get-ADUser -Filter * -SearchBase 'OU=Excluded,DC=ad,DC=thesysadminchannel,DC=com' | `
+select -ExpandProperty SamAccountName
+ 
+#Change UPN Suffix from sub domain to primary domain
+foreach ($User in $UserList) {
+    Get-ADUser $User | Set-ADUser -UserPrincipalName "$User@$Domain"
+}
+ 
+Get-ADUser -Filter * -SearchBase 'OU=Excluded,DC=ad,DC=thesysadminchannel,DC=com' | select Name, UserPrincipalName
