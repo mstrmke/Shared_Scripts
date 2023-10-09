d3e11a7a44b058e4e9d78fb2926248e6ef9375ab
mstrmke <70039031+mstrmke@users.noreply.github.com>
2023-10-09 12:42:33 -0500

    Update Uninstall-HPBloatware.ps1
    Added SilentlyContinue to Uninstsalls

--- Uninstall-HPBloatware.ps1
+++ Uninstall-HPBloatware.ps1
@@ -1,13 +1,13 @@
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security'} -ComputerName . ).Uninstall() 
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
 (Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security - Console'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security Application Support for Sure Sense'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security Application Support for Chrome'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Notifications'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Security Update Service'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP System Default Settings'} -ComputerName . ).Uninstall()
-(Get-WmiObject -Class Win32_Product -Filter {Name='ICS'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Sure Run Module'} -ComputerName . ).Uninstall() 
-(Get-WmiObject -Class Win32_Product -Filter {Name='HP Documentation'} -ComputerName . ).Uninstall() 
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security Application Support for Sure Sense'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Wolf Security Application Support for Chrome'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Notifications'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Security Update Service'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP System Default Settings'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='ICS'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Sure Run Module'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
+(Get-WmiObject -Class Win32_Product -Filter {Name='HP Documentation'} -ComputerName . ).Uninstall() -ErrorAction SilentlyContinue
 
 
 
