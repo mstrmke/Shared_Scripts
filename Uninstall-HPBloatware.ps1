f3cbd6768955ae6a482708be4efa0c4fd40dc65d
mstrmke <hogan.ryant@gmail.com>
2024-03-08 11:28:58 -0600

    Version 1.6

--- Uninstall-HPBloatware.ps1
+++ Uninstall-HPBloatware.ps1
@@ -14,17 +14,17 @@
 #>
 
 #Remove HP Bloatware from Add/Remove Programs
-$HPInstalledApps = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -like "*HP*"}
+# Get a list of all installed applications
 
-foreach ($HPInstalledApp in $HPInstalledApps)
-{
-	$HPWin32Product.Uninstall()
-}
+Get-package -Name "HP Wolf Security"| Uninstall-package -AllVersions -ErrorAction SilentlyContinue
+Get-package -Name "*HP Wolf Security*"| Uninstall-package -AllVersions -ErrorAction SilentlyContinue
+Start-Process "C:\Program Files (x86)\Hewlett-Packard\HP Support Framework\UninstallHPSA.exe" -ArgumentList "/s /v/qn UninstallKeepPreferences=FALSE"
+Get-package -Name "*HP*"| Uninstall-package -AllVersions -ErrorAction SilentlyContinue
 
 #Removing Unneeded HP AppX Bundles
 $HPAppxPackages = Get-AppxPackage -Name *HP*
 
 foreach ($HPAppxPackage in $HPAppxPackages)
 {
-	$HPAppxPackage | Remove-AppxPackage
+	$HPAppxPackage | Remove-AppxPackage -ErrorAction SilentlyContinue
 }
