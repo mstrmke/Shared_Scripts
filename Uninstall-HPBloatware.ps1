96b5d287ed8faa3e96210fa3d21cb6d7d5ee7a53
mstrmke <70039031+mstrmke@users.noreply.github.com>
2024-04-17 17:09:07 -0500

    Version 1.8

--- Uninstall-HPBloatware.ps1
+++ Uninstall-HPBloatware.ps1
@@ -8,6 +8,7 @@
 	 Version: 	1.5 - Added Wilcards to Apx Bundles
 	 			1.6 - Replaced Indivdual Apps with HP Win32 Product Uninstall Loop for all HP  Apps
 				1.7 - Added Error Checking to all Uninstall Commands
+				1.8 - Set ErrorActionPreference to SilentlyContinue for entire Script. Added 'ErrorAction Continue' to all Catch Blocks
 	===========================================================================
     .DESCRIPTION
     This script manually removes an HP Bloatware on ENROLLED devices.
@@ -20,36 +21,36 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 try {
-	Get-Package -Name "HP Wolf Security" | Uninstall-Package -AllVersions $ErrorActionPreference
+	Get-Package -Name "HP Wolf Security" | Uninstall-Package -AllVersions
 	Write-Host "HP Wolf Security has been uninstalled." -ForegroundColor Green
 }
 catch {
-	Write-Error "An error occurred: $_"
+	Write-Error "An error occurred: $_" -ErrorAction Continue
 }
 
 try {
-	Get-Package -Name "*HP Wolf Security*" | Uninstall-Package -AllVersions $ErrorActionPreference
+	Get-Package -Name "*HP Wolf Security*" | Uninstall-Package -AllVersions
 	Write-Host "Remaining components of HP Wolf Security has been uninstalled." -ForegroundColor Green
 }
 catch {
-	Write-Error "An error occurred: $_" 
+	Write-Error "An error occurred: $_" -ErrorAction Continue
 }
 
 try {
-	Start-Process "C:\Program Files (x86)\Hewlett-Packard\HP Support Framework\UninstallHPSA.exe" -ArgumentList "/s /v/qn UninstallKeepPreferences=FALSE" -Wait $ErrorActionPreference
+	Start-Process "C:\Program Files (x86)\Hewlett-Packard\HP Support Framework\UninstallHPSA.exe" -ArgumentList "/s /v/qn UninstallKeepPreferences=FALSE" -Wait
 	Write-Host "HP Support Asisstant has been uninstalled." -ForegroundColor Green
 	
 }
 catch {
-	Write-Error "An error occurred: $_" 
+	Write-Error "An error occurred: $_" -ErrorAction Continue
 }
 
 try {
-	Get-Package -Name "*HP*" | Uninstall-Package -AllVersions $ErrorActionPreference
+	Get-Package -Name "*HP*" | Uninstall-Package -AllVersions
 	Write-Host "All Remaining Applications of *HP* have been uninstalled." -ForegroundColor Green
 }
 catch {
-	Write-Error "An error occurred: $_"
+	Write-Error "An error occurred: $_" -ErrorAction Continue
 }
 
 
@@ -59,7 +60,7 @@ $HPAppxPackages = Get-AppxPackage -Name *HP*
 foreach ($HPAppxPackage in $HPAppxPackages)
 {
 	try {
-		$HPAppxPackage | Remove-AppxPackage $ErrorActionPreference
+		$HPAppxPackage | Remove-AppxPackage 
 		Write-Host "$HPAppxPackage has been uninstalled." -ForegroundColor Green
 	}
 	catch {
